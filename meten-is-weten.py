c2504493bb8949e7755bc2949e9b88fda86600b0
Lucas van Pelt <lucasvpelt03@gmail.com>
2021-09-15 10:11:27 +0200

    Min en Max

--- meten-is-weten.py
+++ meten-is-weten.py
@@ -1,6 +1,8 @@
-a = int(input('Voor je getal in: '))
-b = int(input('Voor je getal in: '))
-if a > b:
-    print('a is het grootste getal')
-elif a < b:
-    print('a is het kleinste getal')
+a = int(input("Voor je getal in: "))
+b = int(input("Voor je getal in: "))
+if b > a:
+  print("B is grooter dan A")
+elif a == b:
+  print("A en B zijn gelijk")
+else:
+  print("A is grooter dan B")
